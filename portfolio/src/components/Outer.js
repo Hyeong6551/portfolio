0fb778af2b38e77915a756a0105fc40cb1a93792
조세형 <137522935+Hyeong6551@users.noreply.github.com>
2025-02-02 21:32:56 +0900

    Delete portfolio directory

--- portfolio/src/components/Outer.js
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-export default function Outer(){
-    return(
-        <div className='container my5'>
-            <Outlet></Outlet>
-        </div>
-    )
-}
